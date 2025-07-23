e0ee7f849f237f92f3fbc887cc35ad9244c3b157
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-22 21:43:16 -0300

    chore: create test for class contructor

--- src/test/java/com/gustavomotamacedo/ai_summarizer/infrastructure/adapter/OpenAIApiAdapterTests.java
+++ src/test/java/com/gustavomotamacedo/ai_summarizer/infrastructure/adapter/OpenAIApiAdapterTests.java
@@ -1,4 +1,16 @@
 package com.gustavomotamacedo.ai_summarizer.infrastructure.adapter;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class OpenAIApiAdapterTests {
+    // Testes unitários para OpenAIApiAdapter
+
+    // Teste para verificar construtor da classe
+    @Test
+    public void testConstructor() {
+        OpenAIApiAdapter adapter = new OpenAIApiAdapter();
+        assertNotNull(adapter);
+    }
 }
