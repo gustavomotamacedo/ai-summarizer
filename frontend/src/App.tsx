851283728545d91daad09e02c6a23f2d5f0084e6
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-26 10:42:28 -0300

    feat: create userInput value in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [userInput, setUserInput] = useState<string>("");
 
   return (
     <>
       <div className='card'>
-        <input type="text" className='text-area' placeholder='Cole o texto a ser resumido.'/>
+        <input type="text" className='text-area' value={userInput} onChange={e => setUserInput(e.target.value)} placeholder='Cole o texto a ser resumido.'/>
         <div>
           <button className='button'>Bullet points</button>
           <button className='button'>Tweet</button>
