ae8bff05e8eaea8c02a37f2da1581e804fff16e3
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-25 22:16:30 -0300

    chore: configure request mapping url

--- src/main/java/com/gustavomotamacedo/ai_summarizer/infrastructure/web/InputTextController.java
+++ src/main/java/com/gustavomotamacedo/ai_summarizer/infrastructure/web/InputTextController.java
@@ -1,4 +1,12 @@
 package com.gustavomotamacedo.ai_summarizer.infrastructure.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${api.base-path}/input")
 public class InputTextController {
+
+
+
 }
