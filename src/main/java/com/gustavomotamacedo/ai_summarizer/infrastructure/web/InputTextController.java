5e79f3baead5692d9b83d9fedb66014d20f07d94
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-26 00:31:10 -0300

    Update InputTextController.java

--- src/main/java/com/gustavomotamacedo/ai_summarizer/infrastructure/web/InputTextController.java
+++ src/main/java/com/gustavomotamacedo/ai_summarizer/infrastructure/web/InputTextController.java
@@ -1,17 +1,14 @@
 package com.gustavomotamacedo.ai_summarizer.infrastructure.web;
 
 import com.gustavomotamacedo.ai_summarizer.domain.model.BulletPointSummary;
+import com.gustavomotamacedo.ai_summarizer.domain.model.TweetSummary;
+import com.gustavomotamacedo.ai_summarizer.domain.model.UserInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.openai.OpenAiChatModel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.http.HttpResponse;
-import java.util.Locale;
 
 @RestController
 @RequestMapping("api/v1")
@@ -19,18 +16,26 @@ public class InputTextController {
 
     private final ChatModel chatModel;
     private final BulletPointSummary bulletPointSummary;
+    private final TweetSummary tweetSummary;
     private static final Logger log = LoggerFactory.getLogger(InputTextController.class);
 
     public InputTextController(ChatModel chatModel) {
         this.chatModel = chatModel;
         this.bulletPointSummary = new BulletPointSummary();
+        this.tweetSummary = new TweetSummary();
     }
 
     @PostMapping("/input")
-    public ResponseEntity<String> inputText(@RequestBody String text) {
+    public ResponseEntity<String> inputText(@RequestBody UserInput userInput) {
         try {
-            if ()
-            String response = this.chatModel.call(bulletPointSummary.applyPrompt(text.toLowerCase()));
+            String response = "";
+            if (userInput.getSummaryType().equals("TWEET")){
+                response = this.chatModel.call(tweetSummary.applyPrompt(userInput.getText().toLowerCase()));
+            } else if (userInput.getSummaryType().equals("BULLET_POINTS")) {
+                response = this.chatModel.call(bulletPointSummary.applyPrompt(userInput.getText().toLowerCase()));
+            } else {
+                throw new Exception("Invalid Summary Type!");
+            }
             return ResponseEntity.ok(response);
         } catch (Exception e) {
             log.error("Error processing chat data: {}", e.getMessage(), e);
