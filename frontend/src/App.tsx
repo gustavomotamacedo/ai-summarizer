40fcc377ea3b1c7278454e77fb1334b2edb2701e
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-26 11:04:43 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,13 +9,37 @@ function App() {
     console.log(aiResponse);
   }, [aiResponse]);
 
+  const fetchAiResponse = async (input: string) => {
+    const API_BASE_URL = "http://localhost:8080/api/v1";
+    const bulletPointsBody = {
+      summaryType: "BULLET_POINTS",
+      text: ""
+    }
+     const tweetBody = {
+      summaryType: "BULLET_POINTS",
+      text: ""
+    }
+    
+    switch (key) {
+      case value:
+        
+        break;
+    
+      default:
+        break;
+    }
+
+    console.log(API_BASE_URL.concat("/input"));
+    console.log(input);
+  }
+
   return (
     <>
       <div className='card'>
         <input type="text" className='text-area' value={userInput} onChange={e => setUserInput(e.target.value)} placeholder='Cole o texto a ser resumido.'/>
         <div>
-          <button className='button' onClick={() => {setAiResponse(userInput)}}>Bullet points</button>
-          <button className='button' onClick={() => {setAiResponse(userInput)}}>Tweet</button>
+          <button className='button' onClick={() => {fetchAiResponse(userInput)}}>Bullet points</button>
+          <button className='button' onClick={() => {fetchAiResponse(userInput)}}>Tweet</button>
         </div>
         <span className='response'>{aiResponse}</span>
       </div>
