3efe5980775e0b2b03d8386b60776f7c75aa124c
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-26 00:31:06 -0300

    Update TweetSummary.java

--- src/main/java/com/gustavomotamacedo/ai_summarizer/domain/model/TweetSummary.java
+++ src/main/java/com/gustavomotamacedo/ai_summarizer/domain/model/TweetSummary.java
@@ -1,5 +1,8 @@
 package com.gustavomotamacedo.ai_summarizer.domain.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class TweetSummary implements ISummary{
     private String prompt;
 
