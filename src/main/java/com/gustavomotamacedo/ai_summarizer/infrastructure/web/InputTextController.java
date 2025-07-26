5afea7cb97b32dffad89034de42a0ffa02e799db
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-25 23:30:04 -0300

    feat: adding AI integration in input endpoint

--- src/main/java/com/gustavomotamacedo/ai_summarizer/infrastructure/web/InputTextController.java
+++ src/main/java/com/gustavomotamacedo/ai_summarizer/infrastructure/web/InputTextController.java
@@ -1,5 +1,11 @@
 package com.gustavomotamacedo.ai_summarizer.infrastructure.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +16,23 @@ import java.util.Locale;
 @RequestMapping("api/v1")
 public class InputTextController {
 
+    private final ChatModel chatModel;
+    private static final Logger log = LoggerFactory.getLogger(InputTextController.class);
+
+    public InputTextController(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
     @PostMapping("/input")
     public ResponseEntity<String> inputText(@RequestBody String text) {
-        return ResponseEntity.ok(text.toLowerCase(Locale.ROOT));
+        try {
+            String response = this.chatModel.call("Say hi to thomas!");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            log.error("Error processing chat data: {}", e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Sorry, an error has occurred with api service.");
+        }
     }
 
 }
