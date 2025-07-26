2f6108365bad73b6e121d9f6805a2dc89a669dc8
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-26 10:45:07 -0300

    feat: create aiResponse variable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
   const [userInput, setUserInput] = useState<string>("");
+  const [aiResponse, setAiResponse] = useState<string>("");
+
+  useEffect(() => {
+    console.log(aiResponse);
+  }, [aiResponse]);
 
   return (
     <>
       <div className='card'>
         <input type="text" className='text-area' value={userInput} onChange={e => setUserInput(e.target.value)} placeholder='Cole o texto a ser resumido.'/>
         <div>
-          <button className='button'>Bullet points</button>
-          <button className='button'>Tweet</button>
+          <button className='button' onClick={() => {setAiResponse(userInput)}}>Bullet points</button>
+          <button className='button' onClick={() => {setAiResponse(userInput)}}>Tweet</button>
         </div>
-        <span className='response'></span>
+        <span className='response'>{aiResponse}</span>
       </div>
     </>
   )
