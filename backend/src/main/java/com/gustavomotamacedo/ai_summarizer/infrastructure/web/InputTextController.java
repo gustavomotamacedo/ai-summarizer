01c302dac5bbff015a4569a569299645408b89fd
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-08-01 11:43:04 -0300

    Update InputTextController.java

--- backend/src/main/java/com/gustavomotamacedo/ai_summarizer/infrastructure/web/InputTextController.java
+++ backend/src/main/java/com/gustavomotamacedo/ai_summarizer/infrastructure/web/InputTextController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:8080")
 public class InputTextController {
 
     private final ChatModel chatModel;
